1ec8b52e7bddd267e758c4163ddf9a9304f149e6
HomuraShu <esy.minmax@gmail.com>
2023-10-03 09:59:37 +0800

    Base React+Threejs

---
+++ src/components/FirstScene.js
@@ -0,0 +1,44 @@
+import React, { useRef, useEffect } from 'react';
+import * as THREE from 'three';
+
+const FirstScene = () => {
+  const sceneRef = useRef();
+
+  useEffect(() => {
+    // Set up scene, camera, and renderer
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const renderer = new THREE.WebGLRenderer();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    sceneRef.current.appendChild(renderer.domElement);
+
+    // Create a cube
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    // Position the camera
+    camera.position.z = 5;
+
+    // Animation function
+    const animate = () => {
+      requestAnimationFrame(animate);
+
+      // Rotate the cube
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+
+      // Render the scene
+      renderer.render(scene, camera);
+    };
+
+    // Start the animation loop
+    animate();
+  }, []);
+
+  return <div ref={sceneRef} />;
+};
+
+export default FirstScene;
